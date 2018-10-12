31c0618e7871d3307cec6563a47fb586685acfdc
Ned Ruggeri <ruggeri@self-loop.com>
2018-10-11 21:53:46 -0700

    build when we start watching

--- scripts/watch.rb
+++ scripts/watch.rb
@@ -2,6 +2,7 @@
 
 require 'open3'
 
+`./scripts/build_all.rb`
 Open3.popen3("fswatch ./entries ./templates") do |stdin, stdout, stderr, status, thread|
   while changed_fname = stdout.gets
     puts "Changed: #{changed_fname}"
