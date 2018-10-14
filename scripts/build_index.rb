34f51dede6adeb1493f5d800da257e3c5d977497
Ned Ruggeri <ruggeri@self-loop.com>
2018-10-13 17:11:42 -0700

    index: list in reverse time; remove time from part of title

--- scripts/build_index.rb
+++ scripts/build_index.rb
@@ -5,7 +5,8 @@ require_relative '../src/entry'
 
 def main
   source = File.read("./entries/index.slim")
-  content = Slim::Template.new { source }.render nil, ({ published_entries: Entry.published })
+  published_entries = Entry.published.sort_by { |e| e.meta. publication_time }.reverse
+  content = Slim::Template.new { source }.render nil, ({ published_entries: published_entries })
   File.open("./index.html", "w") { |f| f.puts content }
 end
 
