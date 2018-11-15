c0f99afb878454e9b2061c3d175abb2236d4216b
Ned Ruggeri <ruggeri@self-loop.com>
2018-11-15 10:03:20 -0800

    reuse head partial in posts index page

--- bin/build_index.rb
+++ bin/build_index.rb
@@ -5,8 +5,17 @@ require_relative '../src/entry'
 
 def main
   source = File.read("./entries/index.slim")
-  published_entries = Entry.published.sort_by { |e| e.meta. publication_time }.reverse
-  content = Slim::Template.new { source }.render nil, ({ published_entries: published_entries })
+  head_source = File.read("./templates/head.slim")
+  head_content = Slim::Template.new { head_source }.render
+
+  published_entries = Entry.published.sort_by do |e|
+    e.meta.publication_time
+  end.reverse
+  content = Slim::Template.new { source }.render(
+    nil, { head_content: head_content,
+           published_entries: published_entries }
+  )
+
   File.open("./index.html", "w") { |f| f.puts content }
 
   # Render login page.
