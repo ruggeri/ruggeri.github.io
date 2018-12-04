e1f15ff3be613423cd0d0c490b6450485d77c911
Ned Ruggeri <ruggeri@self-loop.com>
2018-12-03 19:55:16 -0800

    rewrite bin/watch.rb so it doesn't rebuild *every* file on a single change

--- bin/watch.rb
+++ bin/watch.rb
@@ -1,11 +1,21 @@
 #!/usr/bin/env ruby
 
 require 'open3'
+require_relative '../src/entry'
 
 `./bin/build_all.rb`
 Open3.popen3("fswatch ./entries ./templates") do |stdin, stdout, stderr, status, thread|
   while changed_fname = stdout.gets
     puts "Changed: #{changed_fname}"
-    `./bin/build_all.rb`
+    if ".*/templates".match(changed_fname)
+      puts "Rebuilding all..."
+      `./bin/build_all.rb`
+    elsif res = /.*\/entries\/(.*)\.(yaml|slim)/.match(changed_fname)
+      yaml_file = "./entries/#{res[1]}.yaml"
+      puts "Rebuilding one..."
+      Entry.new(yaml_file).build!
+      puts "Rebuilding index..."
+      `./bin/build_index.rb`
+    end
   end
 end
